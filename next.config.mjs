f8fe711477b325d77b46ec802c9693cdbc6aad74
117Ustim <ustik72@gmail.com>
2025-05-02 04:05:10 +0300

    2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  output: 'export', // Это важно для статической сборки
-  experimental: {
-    appDir: true, // Если ты используешь новую структуру с app/
-  },
+    images: {
+        domains: ['images.unsplash.com'],
+      },
+   
 };
 
 export default nextConfig;
